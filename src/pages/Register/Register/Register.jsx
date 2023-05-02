955c29249923bcd3d1165b6b5c896e203219a028
md jakir hossain <jakirhossainmd540@gmail.com>
2023-05-02 16:00:28 +0600

    routing setup added

---
+++ src/pages/Register/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            this is the Register
+        </div>
+    );
+};
+
+export default Register;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,37 +1,70 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Recepi from "../pages/Recepi/Recepi/Recepi";
 import Feature from "../pages/Feature/Feature/Feature";
 import Blogs from "../pages/Blogs/Blogs/Blogs";
+import SubscribeSection from "../pages/Home/SubscribeSection/SubscribeSection";
+import RecepiesLayout from "../layouts/RecepiesLayout";
+import LoginLayout from "../layouts/LoginLayout";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Register/Register/Register";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children:[
-        {
-            path:"/",
-            element:<Home></Home>
-        },
-        {
-            path:"recepies",
-            element:<Recepi></Recepi>
-        },
-        {
-            path:"features",
-            element:<Feature></Feature>
-        },
-        {
-            path:"blogs",
-            element:<Blogs></Blogs>
-        },
-      ]
-    },
-  ]);
+      {
+        path: "features",
+        element: <Feature></Feature>,
+      },
+      {
+        path: "subscribeSection",
+        element: <SubscribeSection></SubscribeSection>,
+      },
+      {
+        path: "blogs",
+        element: <Blogs></Blogs>,
+      },
+    ],
+  },
 
-  export default router;
+  {
+    path: "/",
+    element: <RecepiesLayout></RecepiesLayout>,
+    children:[
+      {
+        path:'recepies',
+        element:<Recepi></Recepi>
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <LoginLayout></LoginLayout>,
+    children:[
+      {
+        path:'/login',
+        element:<Login></Login>
+      }
+    ]
+  },
+  {
+    path: "register",
+    element: <LoginLayout></LoginLayout>,
+    children:[
+      {
+        path:'/register',
+        element:<Register></Register>
+      }
+    ]
+  },
+]);
+
+export default router;
